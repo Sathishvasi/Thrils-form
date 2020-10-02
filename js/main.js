b614f292c48c0ec41817b27ed649f8bc3612f523
sathishvasi <sathish@wizroots.com>
2020-10-02 23:38:46 +0530

    Latest changes of form

--- js/main.js
+++ js/main.js
@@ -12,7 +12,17 @@
             else {
                 $(this).parent().addClass('true-validate');
             }
-        })    
+        });
+        
+        let radioInput = $(this).find('input');
+         $(radioInput).on('blur', function(){
+            if(validate(this) == false){
+                showValidate(this);
+            }
+            else {
+                $(this).closest('.validate-input').addClass('true-validate');
+            }
+        });
     })
   
   
@@ -29,23 +39,71 @@
                 check=false;
             }
         }
+        if(check){
+            let formEle = $('.contact100-form').serializeArray();
+            let createObj = [];
+            formEle.forEach((v,i)=>{
+                let header = v.name;
+                let value = v.value;
+                createObj[header] = value;
+            });
+            console.log(createObj)
 
-        return check;
+            $('.contact100-form')[0].reset();
+            $('.validate-input').removeClass('true-validate');
+            $('#snackbar').text("Your Form Submitted Successfully!").addClass('show');
+        }else{
+            $("html, body").animate({ scrollTop: "0" }, 2000);
+            $('#snackbar').text("Please fill all the fields!").addClass('show');
+        }
+
+        setTimeout(function(){ 
+            $('#snackbar').removeClass('show');
+        }, 3000);
+
+        return false;
     });
 
+
+    $('input[name="time"]').click(function(){
+        $('input[name="time-input"]').val('');
+    });
 
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
            $(this).parent().removeClass('true-validate');
         });
+
+        let radioInput = $(this).find('input');
+        $(radioInput).focus(function(){
+            hideValidate($(this));
+            $(this).parent().removeClass('true-validate');
+         });
     });
+
+    function validateRadioEle(input){
+        if($(input).is(':checked')){
+            return true;
+        }
+    }
 
      function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
+        }
+        else if($(input).hasClass('radio-group')){
+            let radioChecked;
+            let radioInput = $(input).find('input');
+            for(var i = 0; i< radioInput.length;i++){
+                radioChecked = validateRadioEle($(radioInput[i]));
+                if(radioChecked === true){
+                    break;
+                }
+            }
+            return radioChecked ? radioChecked : false;
         }
         else {
             if($(input).val().trim() == ''){
@@ -55,7 +113,7 @@
     }
 
     function showValidate(input) {
-        var thisAlert = $(input).parent();
+        var thisAlert = $(input).closest('.validate-input');
 
         $(thisAlert).addClass('alert-validate');
 
@@ -68,7 +126,7 @@
     }
 
     function hideValidate(input) {
-        var thisAlert = $(input).parent();
+        var thisAlert = $(input).closest('.validate-input');
         $(thisAlert).removeClass('alert-validate');
         $(thisAlert).find('.btn-hide-validate').remove();
     }
